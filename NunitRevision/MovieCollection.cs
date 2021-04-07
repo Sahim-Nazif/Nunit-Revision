3c20d73f9f4024375ae1f0494d5a74dce687a289
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-04-07 17:54:57 -0400

    add movie collection class

---
+++ NunitRevision/MovieCollection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitRevision
+{
+    public class MovieCollection
+    {
+        private int inventory;
+
+        public MovieCollection()
+        {
+
+        }
+        public MovieCollection(int inventory)
+        {
+            this.inventory = inventory;
+        }
+
+        public int Inventory
+        {
+            get { return inventory; }
+        }
+    }
+}
