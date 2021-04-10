c238784297d94aeb20888bd0268e74d9b102205e
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-04-09 23:33:45 -0400

    tested method selling-renting more than inventory

--- MovieUnitTest/MovieCollectTest.cs
+++ MovieUnitTest/MovieCollectTest.cs
@@ -31,12 +31,37 @@ namespace MovieUnitTest
             //Assert
             Assert.AreEqual(200, movie.Inventory);
         }
+        [Test]
+        public void Transfer_Movies_To_OtherStore_UpdatesBothAccount()
+        {
 
+            //Arrange
+            var movie = new MovieCollection(500);
+            var otherStore = new MovieCollection();
+            //Act
+            movie.TransferToOtherStores(otherStore,200);
+
+            //Assert
+            Assert.AreEqual(300, movie.Inventory);
+            Assert.AreEqual(200, otherStore.Inventory);
+
+
+        }
+        [Test]
+        public void SellingRent_More_Than_Inventory_Throws()
+        {
+            //Arrange
+            var movieInventory = new MovieCollection(500);
+            //Act & Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => movieInventory.SoldOrRent(600));
+
+        }
         [Test]
         public void Add_Negative_Number_Throws()
         {
-
+            //Arrange
             var movie = new MovieCollection(500);
+            //Act + Assert
             Assert.Throws<ArgumentOutOfRangeException>(() => movie.Add(-100));
 
 
